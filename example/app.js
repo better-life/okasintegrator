c97dcd313d7887c6acbf3fc229fac2aafa7870a3
Anil <b.anil@better-life.co.in>
2015-12-12 17:47:57 +0530

    added init for auto sync every 1 hour

--- example/app.js
+++ example/app.js
@@ -1,16 +1,20 @@
 var Integrator = require('okasintegrator');
 var integrator = new Integrator({
 	connectionString:"", //mongodb connection uri
-	parserPath:__dirname+"/parser/" //path to folder where all the parsers are kept
+	parserPath:__dirname+"/parser/", //path to folder where all the parsers are kept
+	cronTime:'0 0 0-23 * * *', //optional defaults to every 1 hour.
 });
 
 var provider_id = "1", entity="CENTERS";
 
+/*METHOD 1*/
 integrator.syncNow(provider_id,entity,function(err){
 	if(err)
 		console.log(err);
 	console.log('Callback from app.js');
 })
+/*METHOD 2*/
+integrator.init();
 
 /*Sample schema for IntegratorConfigs collection*/
 /*
